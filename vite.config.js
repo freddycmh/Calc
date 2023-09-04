f59620f25779e02ad9c0a46623bae404ea8b1af8
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2023-09-04 21:20:32 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+// Vite configuration
 export default defineConfig({
-  plugins: [react({ base: "/Calc" })],
+  plugins: [react({ base: "/Calc/" })],
+
+  // ... other config settings
 });
