d664c3cb2f3f32e2ce6de9a73b5934bea4a3b927
Harun Biswas Rubel <harunbiswasrubel@gmail.com>
2023-09-04 21:55:48 +0600

    update

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,9 @@
-export default {
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// Vite configuration
+export default defineConfig({
+  plugins: [react()],
+
   // ... other config settings
-  base: "/Calc/", // Replace with your repository name
-};
+});
